90ba4a71908fcd9194fbaca6060d7f493922e2b8
Michel <Michel_motongi@hotmail.com>
2021-12-02 21:16:50 -0500

    Iss activity

--- iss.js
+++ iss.js
@@ -1,85 +1,85 @@
 const request = require("request");
 const nextISSTimesForMyLocation = function(callback) {
-    fetchMyIP((error, ip) => {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+  
+    fetchCoordsByIP(ip, (error, coords) => {
       if (error) {
         return callback(error, null);
       }
   
-      fetchCoordsByIP(ip, (error, coords) => {
+      fetchISSFlyOverTimes(coords, (error, nextPasses) => {
         if (error) {
           return callback(error, null);
         }
   
-        fetchISSFlyOverTimes(coords, (error, nextPasses) => {
-          if (error) {
-            return callback(error, null);
-          }
-  
-          callback(null, nextPasses);
-        });
+        callback(null, nextPasses);
       });
     });
+  });
 };
-const fetchMyIP = function(callback) { 
-    // use request to fetch IP address from JSON API
-    request('https://api.ipify.org?format=json', (error,response,body) => {
+const fetchMyIP = function(callback) {
+  // use request to fetch IP address from JSON API
+  request('https://api.ipify.org?format=json', (error,response,body) => {
 
-        if (error) return callback(error, null);
-          // if non-200 status, assume server error
-        if (response.statusCode !== 200) {
-            callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
-            return;
-        }
+    if (error) return callback(error, null);
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
 
-        const ip = JSON.parse(body).ip;
-        callback(null,ip);
-    });  
+    const ip = JSON.parse(body).ip;
+    callback(null,ip);
+  });
 };
-const fetchCoordsByIP = function(ip,callback){
-    request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+const fetchCoordsByIP = function(ip,callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
     
-        if (error) {
-            callback(error, null);
-            return;
-        }
-      // if non-200 status, assume server error
-        if (response.statusCode !== 200) {
-            callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
-            return;
-        }
-        const { latitude, longitude }  = JSON.parse(body);
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+    const { latitude, longitude }  = JSON.parse(body);
 
-        callback(null,{ latitude, longitude});
-    })
-}
+    callback(null,{ latitude, longitude});
+  });
+};
 const fetchISSFlyOverTimes = function(coords, callback) {
-    // ...
-    request(`https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`, (error,response,body) => {
+  // ...
+  request(`https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`, (error,response,body) => {
 
     if (error) {
-        callback(error, null);
-        return;
+      callback(error, null);
+      return;
     }
-  // if non-200 status, assume server error
+    // if non-200 status, assume server error
     if (response.statusCode !== 200) {
-        callback(Error(`Status Code ${response.statusCode} when fetching the latitude and/or the longitude: ${body}`), null);
-        return;
+      callback(Error(`Status Code ${response.statusCode} when fetching the latitude and/or the longitude: ${body}`), null);
+      return;
     }
     const passes = JSON.parse(body).response;
 
     callback(null, passes);
 
-    });
+  });
 };
 /**
  * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
  * Input:
- *   - A callback with an error or results. 
+ *   - A callback with an error or results.
  * Returns (via Callback):
  *   - An error, if any (nullable)
  *   - The fly-over times as an array (null if error):
  *     [ { risetime: <number>, duration: <number> }, ... ]
- */ 
+ */
 
 
 module.exports = { nextISSTimesForMyLocation };
